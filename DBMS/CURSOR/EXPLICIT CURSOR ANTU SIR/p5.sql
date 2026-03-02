8e55463a7103363709e84416cf8b6e033059a060
sanbisensei <lsunbi1@gmail.com>
2026-03-03 00:30:56 +0600

    Implement delete operation for instructor and add output messages for found/not found cases

--- DBMS/CURSOR/EXPLICIT CURSOR ANTU SIR/p5.sql
+++ DBMS/CURSOR/EXPLICIT CURSOR ANTU SIR/p5.sql
@@ -3,8 +3,13 @@ set serveroutput on;
 declare
 
 BEGIN
-  
+  DELETE from instructor
+  where id = 12345;
 
-    DBMS_OUTPUT.PUT_LINE();
+  if SQL%NOTFOUND THEN
+    DBMS_OUTPUT.PUT_LINE('not found');
+    else
+    DBMS_OUTPUT.PUT_LINE('found');
+    end if;
 END;
 /
