de66f29994b7206ea6db134f464df4016082dd08
sanbisensei <lsunbi1@gmail.com>
2026-03-03 00:28:18 +0600

    p4 done

--- DBMS/CURSOR/EXPLICIT CURSOR ANTU SIR/p4.sql
+++ DBMS/CURSOR/EXPLICIT CURSOR ANTU SIR/p4.sql
@@ -1,10 +1,23 @@
 set serveroutput on;
 
 declare
+    cursor ic is
+    select id,name,dept_name,salary
+    from instructor;
 
+    idy instructor.id%TYPE;
+    namey instructor.name%TYPE;
+    depty instructor.dept_name%TYPE;
+    salaryy instructor.salary%TYPE;
 BEGIN
-  
+    open ic;
+    
+    loop
+    fetch ic into idy,namey,depty,salaryy;
+    exit when ic%NOTFOUND;
 
-    DBMS_OUTPUT.PUT_LINE();
+    DBMS_OUTPUT.PUT_LINE(idy||namey||depty||salaryy);
+    end loop;
+close ic;
 END;
 /
