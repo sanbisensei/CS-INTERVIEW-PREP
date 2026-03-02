8bb1084375b39997396c7986796d230ce0e8573c
sanbisensei <lsunbi1@gmail.com>
2026-03-03 00:47:30 +0600

    p6 done

--- DBMS/CURSOR/EXPLICIT CURSOR ANTU SIR/p6.sql
+++ DBMS/CURSOR/EXPLICIT CURSOR ANTU SIR/p6.sql
@@ -1,10 +1,25 @@
 set serveroutput on;
 
 declare
+cursor ic is
+select id,salary from instructor where salary <60000;
+
+idy instructor.id%TYPE;
+salaryy instructor.salary%TYPE;
+cnt number :=0;
 
 BEGIN
-  
+open ic;
+loop
+    fetch ic into idy,salaryy;
+    exit when ic%NOTFOUND;
 
-    DBMS_OUTPUT.PUT_LINE();
+    update instructor
+    set salary = salary +1000
+    where id = idy;
+    cnt:= cnt+1;
+end loop;
+close ic;
+    DBMS_OUTPUT.PUT_LINE(cnt);
 END;
 /
