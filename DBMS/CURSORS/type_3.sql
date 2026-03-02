62e64c269a6c9d5123b976f24ba3c56699c46da7
sanbisensei <lsunbi1@gmail.com>
2026-03-02 21:33:20 +0600

    type

--- DBMS/CURSORS/type_3.sql
+++ DBMS/CURSORS/type_3.sql
@@ -2,7 +2,14 @@
 SET SERVEROUTPUT ON;
 SET VERIFY OFF;
 
+Declare
+v_name employee.name%type;
+v_salary employee.salary%type;
 Begin
+select name,salary into v_name,v_salary
+from employee
+where E_ID=2;
 
+DBMS_OUTPUT.PUT_LINE('Name:'|| v_name|| 'salary:' || v_salary);
 end;
 /
